c618eec8c00a1523c43272e7a5da8ea74b1d79ac
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2022-11-03 00:36:24 +0800

    finishing 10 projects

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -3,9 +3,62 @@ import Loading from './Loading'
 import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+
+
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url)
+      const tours = await response.json();
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log("Error " + error);
+    }
+
+
+  }
+
+  // fetch once only
+  useEffect(() => {
+    fetchTours();
+
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  return (
+    <main>
+      {tours.length === 0 ? (
+        <main>
+          <div className='title'>
+            <h2>No Tours Left</h2>
+            <button onClick={() => fetchTours()}>Fetch</button>
+          </div>
+        </main>
+      ) : <Tours tours={tours} removeTour={removeTour} />}
+
+    </main>
+  )
 }
 
 export default App
